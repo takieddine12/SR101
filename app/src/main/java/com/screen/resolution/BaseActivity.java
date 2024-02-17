d61915058b4f6381054ba832aba6db4c5b3a89b3
takieddine12 <algeriahero22@gmail.com>
2024-02-17 19:07:26 +0100

    new changes

--- app/src/main/java/com/screen/resolution/BaseActivity.java
+++ app/src/main/java/com/screen/resolution/BaseActivity.java
@@ -10,13 +10,77 @@ import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.WindowManager;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
+    private static final int[] ORDERED_DENSITY_DP = {
+            DisplayMetrics.DENSITY_LOW,
+            DisplayMetrics.DENSITY_MEDIUM,
+            DisplayMetrics.DENSITY_TV,
+            DisplayMetrics.DENSITY_HIGH,
+            DisplayMetrics.DENSITY_260,
+            DisplayMetrics.DENSITY_280,
+            DisplayMetrics.DENSITY_XHIGH,
+            DisplayMetrics.DENSITY_300,
+            DisplayMetrics.DENSITY_600,
+            DisplayMetrics.DENSITY_520,
+            DisplayMetrics.DENSITY_450,
+            DisplayMetrics.DENSITY_340,
+            DisplayMetrics.DENSITY_360,
+            DisplayMetrics.DENSITY_400,
+            DisplayMetrics.DENSITY_420,
+            DisplayMetrics.DENSITY_440,
+            DisplayMetrics.DENSITY_XXHIGH,
+            DisplayMetrics.DENSITY_560,
+            DisplayMetrics.DENSITY_XXXHIGH,
+    };
+
+
+    @Override
+    protected void attachBaseContext(final Context baseContext) {
+
+        Context newContext = baseContext;
+
+        // Screen zoom is supported from API 24+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+            Resources resources = baseContext.getResources();
+            DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+            Configuration configuration = resources.getConfiguration();
+
+
+            if (displayMetrics.densityDpi != DisplayMetrics.DENSITY_DEVICE_STABLE) {
+                if(Settings.Global.getString(baseContext.getContentResolver(), "display_size_forced") != null) {
+                    float defaultDensity = (DisplayMetrics.DENSITY_DEVICE_STABLE / (float) DisplayMetrics.DENSITY_DEFAULT);
+                    float defaultScreenWidthDp = displayMetrics.widthPixels / defaultDensity;
+                    configuration.densityDpi = findDensityDpCanFitScreen((int) defaultScreenWidthDp);
+                } else {
+                    configuration.densityDpi = DisplayMetrics.DENSITY_DEVICE_STABLE;
+                }
+                configuration.fontScale = 1.0f;
+                newContext = baseContext.createConfigurationContext(configuration);
+            }
+
+        }
+        super.attachBaseContext(newContext);
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private static int findDensityDpCanFitScreen(final int densityDp) {
+        int[] orderedDensityDp = ORDERED_DENSITY_DP;
+        int index = 0;
+        while (densityDp >= orderedDensityDp[index]) {
+            index++;
+        }
+        return orderedDensityDp[index];
+    }
+}
+
+
+/*
     @TargetApi(Build.VERSION_CODES.N)
     private static final int[] ORDERED_DENSITY_DP_N = {
             DisplayMetrics.DENSITY_LOW,
@@ -94,54 +158,4 @@ public class BaseActivity extends AppCompatActivity {
             DisplayMetrics.DENSITY_XXXHIGH,
     };
 
-
-    @Override
-    protected void attachBaseContext(final Context baseContext) {
-
-        Context newContext = baseContext;
-
-        // Screen zoom is supported from API 24+
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-
-            Resources resources = baseContext.getResources();
-            DisplayMetrics displayMetrics = resources.getDisplayMetrics();
-            Configuration configuration = resources.getConfiguration();
-
-
-            if (displayMetrics.densityDpi != DisplayMetrics.DENSITY_DEVICE_STABLE) {
-                if(Settings.Global.getString(baseContext.getContentResolver(), "display_size_forced") != null) {
-                    float defaultDensity = (DisplayMetrics.DENSITY_DEVICE_STABLE / (float) DisplayMetrics.DENSITY_DEFAULT);
-                    float defaultScreenWidthDp = displayMetrics.widthPixels / defaultDensity;
-                    configuration.densityDpi = findDensityDpCanFitScreen((int) defaultScreenWidthDp);
-                } else {
-                    configuration.densityDpi = DisplayMetrics.DENSITY_DEVICE_STABLE;
-                }
-                configuration.fontScale = 1.0f;
-                newContext = baseContext.createConfigurationContext(configuration);
-            }
-
-        }
-        super.attachBaseContext(newContext);
-    }
-
-    @TargetApi(Build.VERSION_CODES.N)
-    private static int findDensityDpCanFitScreen(final int densityDp) {
-        int[] orderedDensityDp;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
-            orderedDensityDp = ORDERED_DENSITY_DP_Q;
-        } else if(Build.VERSION.SDK_INT == Build.VERSION_CODES.P) {
-            orderedDensityDp = ORDERED_DENSITY_DP_P;
-        } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-            orderedDensityDp = ORDERED_DENSITY_DP_N_MR1;
-        } else {
-            orderedDensityDp = ORDERED_DENSITY_DP_N;
-        }
-
-        int index = 0;
-        while (densityDp >= orderedDensityDp[index]) {
-            index++;
-        }
-        return orderedDensityDp[index];
-    }
-}
+ */
